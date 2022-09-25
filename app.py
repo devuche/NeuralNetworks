f3bced63ca1238914bf48efe3c5a1fb477f7018d
Uchenna Nnamani <nnamaniuchenna8@gmail.com>
2022-09-25 21:38:34 +0100

    Final commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 
 import nltk
+nltk.download('popular')
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import load_model
@@ -17,20 +18,20 @@ model = load_model('chatbotmodel.h5')
 
 def clean_uo_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-def bag_of_words(sentence):
+def bag_of_words(sentence, words):
     sentence_words = clean_uo_sentence(sentence)
     bag = [0] * len(words)
     for w in sentence_words:
         for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
-    return  np.array(bag)
+    return(np.array(bag))
 
-def predict_class(sentence):
-    bow = bag_of_words(sentence)
+def predict_class(sentence, model):
+    bow = bag_of_words(sentence, words)
     res = model.predict(np.array([bow]))[0]
     ERROR_TRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_TRESHOLD]
@@ -50,15 +51,22 @@ def get_response(intents_list, intents_json):
             break
     return result
 
-print("I am Mr Hill, I can solve all your UMIS problems")
-
-while True:
-    message = input("")
-    ints = predict_class(message)
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
     res = get_response(ints, intents)
-    print(res)
-    if message.lower=="bye" or message.lower=="goodbye":
-        print("Goodbye and have a blessed day!")
-    break
+    return res
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+if __name__ == "__main__":
+    app.run()
 
 
